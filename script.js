e84001161e7606a8341e70c58bbdf2fa255ad97b
iammoses <inawo.moses@gmail.com>
2023-08-01 02:26:00 +0100

    project completed

---
+++ script.js
@@ -0,0 +1,20 @@
+const bar = document.getElementById('bar')
+const sidebar = document.getElementById('sidebar')
+const body = document.getElementById('body')
+
+bar.addEventListener('click', () =>{
+    if (bar.classList.contains('fa-bars') && body.classList.contains('body')) {
+        bar.classList.remove('fa-bars')
+        bar.classList.add('fa-times')
+        body.classList.remove('body')
+        body.classList.add('body-2')
+        sidebar.style.display = 'none'
+
+    } else if(bar.classList.contains('fa-times') && body.classList.contains('body-2')){
+        bar.classList.remove('fa-times')
+        bar.classList.add('fa-bars')
+        body.classList.remove('body-2')
+        body.classList.add('body')
+        sidebar.style.display = 'flex'
+    }
+})
